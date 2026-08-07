a2c204a6df9e468070b3d82a85f7c64c7a20eeae
yasima-csiro <yasima.kankanamge@csiro.au>
2026-08-07 11:03:40 +1000

    Fix s3 issues

--- image-service/grails-app/init/au/org/ala/images/Application.groovy
+++ image-service/grails-app/init/au/org/ala/images/Application.groovy
@@ -38,6 +38,7 @@ import java.lang.reflect.Constructor
 import java.lang.reflect.InvocationTargetException
 import java.util.ServiceLoader
 import java.util.concurrent.Executor
+import java.util.concurrent.ThreadPoolExecutor
 
 //@EnableConfigurationProperties(ImageOptimisationConfig)
 @Slf4j
@@ -115,7 +116,11 @@ class Application extends GrailsAutoConfiguration {
 
     @Bean
     TaskExecutor derivativeLoaderExecutor() {
-        return createThreadPoolTaskExecutor("derivative-loader-", derivativeLoaderThreads, derivativeLoaderThreads, Math.max(0, derivativeLoaderQueueCapacity))
+        ThreadPoolTaskExecutor executor = createThreadPoolTaskExecutor("derivative-loader-", derivativeLoaderThreads, derivativeLoaderThreads, Math.max(0, derivativeLoaderQueueCapacity)) as ThreadPoolTaskExecutor
+        // Derivative requests are user-facing. When the bounded executor is full, apply
+        // backpressure on the request thread instead of rejecting every subsequent image.
+        executor.rejectedExecutionHandler = new ThreadPoolExecutor.CallerRunsPolicy()
+        return executor
     }
 
     @Bean
